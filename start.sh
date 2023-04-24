267a614a52580028c1e08e0c6d5224c10747bdba
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2023-04-24 20:06:37 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MAULLIKPATEL/ShortenerBot-1/tree/try1 /ShortenerBot-1
+  git clone https://github.com/MAULLIKPATEL/ShortenerBot-1/tree/try1.git /ShortenerBot-1
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /ShortenerBot-1
