b6fce518edf40b4b9aa039e90d2a49149ebb82a8
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2023-07-18 18:52:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -21,7 +21,26 @@ logger = logging.getLogger(__name__)
 BATCH_FILES = {}
 
 
-
+@Client.on_message(filters.command("set_api") & filters.incoming & filters.group)
+async def set_chat_api_cmd(c: Client, msg: Message):
+    chat_member = await c.get_chat_member(
+        chat_id=msg.chat.id,
+        user_id=msg.from_user.id
+    )
+    if not chat_member.status.OWNER:
+        return await msg.reply_text(
+            "You are not chat owner !!", quote=True
+        )
+    if len(msg.text.split()) == 1:
+        return await msg.reply_text("pass API after /set_chat_api command !!", quote=True)
+    await chat_db.set_api(chat_id=msg.chat.id, api=msg.text.split()[-1])
+    return await msg.reply_text(
+        "**!! API Set for this Chat !!**\n\n"
+        f"**API:** `{msg.text.split()[-1]}`\n"
+        f"**CHAT ID:** `{msg.chat.id}`",
+        quote=True
+    )
+    
 
 
 @Client.on_message(filters.command("set_chat_site") & filters.incoming & filters.group & filters.user(ADMINS))
